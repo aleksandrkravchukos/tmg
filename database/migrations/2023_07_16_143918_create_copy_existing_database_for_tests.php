b7795568ff2983b267cdbc0bb0354f4877fc88e6
Aleksandr Kravchuk <kravchuk.aleksandr@makeup.com.ua>
2023-07-16 19:32:00 +0300

    Added test database.

--- database/migrations/2023_07_16_143918_create_copy_existing_database_for_tests.php
+++ database/migrations/2023_07_16_143918_create_copy_existing_database_for_tests.php
@@ -3,27 +3,40 @@
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Support\Facades\Schema;
 
-return new class extends Migration
-{
+return new class extends Migration {
     /**
      * Run the migrations.
      */
     public function up(): void
     {
-        DB::connection('mysql_test')->statement('CREATE DATABASE IF NOT EXISTS `tmg_test`');
-        DB::connection('mysql_test')->statement('SET FOREIGN_KEY_CHECKS=0');
+        DB::connection('mysql')->statement('DROP DATABASE IF EXISTS tmg_test');
+        DB::connection('mysql')->statement('CREATE DATABASE IF NOT EXISTS tmg_test');
+
+        // Create the 'users' table
+        DB::connection('mysql_test')->statement(
+            'CREATE TABLE `users` (
+        `id` INT AUTO_INCREMENT PRIMARY KEY,
+        `name` VARCHAR(255),
+        `email` VARCHAR(255) UNIQUE,
+        `email_verified_at` TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+        `phone` VARCHAR(255),
+        `password` VARCHAR(255),
+        `remember_token` VARCHAR(255) NULL,
+        `created_at` TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+        `updated_at` TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP)'
+        );
+
         DB::connection('mysql_test')->beginTransaction();
 
-        $tables = ['users', 'migrations'];
+        $tables = ['users'];
 
         foreach ($tables as $table) {
             $query = "INSERT INTO `tmg_test`.`$table` SELECT * FROM `tmg`.`$table`";
-            DB::connection('mysql_test')->statement($query);
+            DB::connection('mysql')->statement($query);
         }
 
         DB::connection('mysql_test')->commit();
         DB::connection('mysql_test')->statement('SET FOREIGN_KEY_CHECKS=1');
-
     }
 
     /**
